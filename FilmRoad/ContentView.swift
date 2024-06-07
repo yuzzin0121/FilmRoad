5191550466deea26c050fba97e0f407501b65618
yujin <ydbwlsjki60@gmail.com>
2024-06-07 11:43:39 +0900

    Initial Commit

---
+++ FilmRoad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FilmRoad
+//
+//  Created by 조유진 on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FilmRoad/FilmRoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmRoadApp.swift
+//  FilmRoad
+//
+//  Created by 조유진 on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FilmRoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
